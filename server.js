4a8844bf1ea74a4d20066057ba97cff9301e1656
emirx422 <161278542+emirx422@users.noreply.github.com>
2025-08-14 05:49:34 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  socket.on("offer", (data) => socket.broadcast.emit("offer", data));
+  socket.on("answer", (data) => socket.broadcast.emit("answer", data));
+  socket.on("ice-candidate", (data) => socket.broadcast.emit("ice-candidate", data));
+});
+
+http.listen(process.env.PORT || 3000, () => console.log("Server çalışıyor..."));
